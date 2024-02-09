ab65330841322a4badff8b2dffa767ba3c373092
Nathan <savardnm@gmail.com>
2024-02-09 11:48:47 +0100

    run headless

--- src/coppelia.py
+++ src/coppelia.py
@@ -9,6 +9,8 @@ import threading
 from pathlib import Path
 from ctypes import *
 
+copp_lib_dir = "/home/nathan/Programs/CoppeliaSim/CoppeliaSim_Edu_V4_6_0_rev10_Ubuntu20_04/"
+
 def simStart():
     if sim.getSimulationState() == sim.simulation_stopped:
         sim.startSimulation()
@@ -24,12 +26,13 @@ def simStop(simLoop):
         sim.stopSimulation()
         simLoop(None, 0)
 
-def simThreadFunc(appDir):
+def simThreadFunc(app_dir, scene, lock=None):
+    if lock != None:
+        lock.acquire()
+        
     from coppeliasim.lib import simInitialize, simDeinitialize, simLoop
     import coppeliasim.bridge
-
-    simInitialize(c_char_p(appDir.encode('utf-8')), 0)
-
+    simInitialize(c_char_p(app_dir.encode('utf-8')), 0)
     coppeliasim.bridge.load()
 
     # fetch CoppeliaSim API sim-namespace functions:
@@ -41,8 +44,12 @@ def simThreadFunc(appDir):
     print('CoppeliaSim version is:', version)
 
     # example: load a scene, run the simulation for 1000 steps, then quit:
-    sim.loadScene('/home/nathan/Programs/CoppeliaSim/CoppeliaSim_Edu_V4_6_0_rev10_Ubuntu20_04/scenes/stickbug/05_Pole_Test.ttt')
+    sim.loadScene(scene)
     simStart()
+
+    if lock != None:
+        lock.release()
+
     for i in range(10000):
         t = sim.getSimulationTime()
         print(f'Simulation time: {t:.2f} [s] (simulation running synchronously to client, i.e. stepped)')
@@ -50,30 +57,37 @@ def simThreadFunc(appDir):
     simStop(simLoop)
     simDeinitialize()
 
-
-def simulate(headless=False, true_headless=False):
-    from coppeliasim.lib import simRunGui
+def get_gui_options(headless, true_headless):
     sim_gui_all = 0x0ffff
     sim_gui_headless = 0x10000
+
     if headless or true_headless:
         options = sim_gui_headless
     else:
         options = sim_gui_all
-    # options = coppeliasim.cmdopt.parse(args)
+    
+    return options
+
+def simulate(headless=False, true_headless=False, **kwargs):
+    coppeliasim_library = get_library(true_headless=False)
+    
+    sys.path.append(copp_lib_dir)
+    from coppeliasim.lib import simRunGui
+
+    # TODO: Bug: Cannot use true headless >:(
+    options = get_gui_options(headless=headless, true_headless=true_headless)
 
     appDir = os.path.dirname(coppeliasim_library)
 
     if true_headless:
-        simThreadFunc(appDir)
+        simThreadFunc(appDir, **kwargs)
     else:
-        t = threading.Thread(target=simThreadFunc, args=(appDir,))
+        t = threading.Thread(target=simThreadFunc, args=(appDir,), kwargs=kwargs)
         t.start()
         simRunGui(options)
         t.join()
 
 def get_library(true_headless=False):
-    copp_lib_dir = "/home/nathan/Programs/CoppeliaSim/CoppeliaSim_Edu_V4_6_0_rev10_Ubuntu20_04/"
-    sys.path.append(copp_lib_dir)
     defaultLibNameBase = 'coppeliaSim'
     if true_headless:
         defaultLibNameBase = 'coppeliaSimHeadless'
@@ -93,6 +107,8 @@ def get_library(true_headless=False):
     import builtins
     builtins.coppeliasim_library = coppeliasim_library
 
+    return coppeliasim_library
+
 if __name__ == '__main__':
-    get_library(true_headless=True)
-    simulate()
+    pole_scene = '/home/nathan/Programs/CoppeliaSim/CoppeliaSim_Edu_V4_6_0_rev10_Ubuntu20_04/scenes/stickbug/05_Pole_Test.ttt'
+    simulate(scene=pole_scene, headless=True)
