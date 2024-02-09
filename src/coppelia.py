c773a0f445d086f4c349d162377ccf0044a6050c
Nathan <savardnm@gmail.com>
2024-02-09 11:33:13 +0100

    clean copp library

--- src/coppelia.py
+++ src/coppelia.py
@@ -2,7 +2,6 @@
 # to avoid libraries load errors
 
 import argparse
-import builtins
 import os
 import sys
 import threading
@@ -10,23 +9,23 @@ import threading
 from pathlib import Path
 from ctypes import *
 
-
 def simStart():
     if sim.getSimulationState() == sim.simulation_stopped:
         sim.startSimulation()
 
-def simStep():
+def simStep(simLoop):
     if sim.getSimulationState() != sim.simulation_stopped:
         t = sim.getSimulationTime()
         while t == sim.getSimulationTime():
             simLoop(None, 0)
 
-def simStop():
+def simStop(simLoop):
     while sim.getSimulationState() != sim.simulation_stopped:
         sim.stopSimulation()
         simLoop(None, 0)
 
 def simThreadFunc(appDir):
+    from coppeliasim.lib import simInitialize, simDeinitialize, simLoop
     import coppeliasim.bridge
 
     simInitialize(c_char_p(appDir.encode('utf-8')), 0)
@@ -47,59 +46,13 @@ def simThreadFunc(appDir):
     for i in range(10000):
         t = sim.getSimulationTime()
         print(f'Simulation time: {t:.2f} [s] (simulation running synchronously to client, i.e. stepped)')
-        simStep()
-    simStop()
+        simStep(simLoop)
+    simStop(simLoop)
     simDeinitialize()
 
-    # # example: simply run CoppeliaSim:
-    # while not simGetExitRequest():
-    #     simLoop(None, 0)
-    # simDeinitialize()
 
-if __name__ == '__main__':
-    copp_lib_dir = "/home/nathan/Programs/CoppeliaSim/CoppeliaSim_Edu_V4_6_0_rev10_Ubuntu20_04/"
-    sys.path.append(copp_lib_dir)
-    # import coppeliasim.cmdopt
-    # parser = argparse.ArgumentParser(description='CoppeliaSim client.', add_help=False)
-    # coppeliasim.cmdopt.add(parser)
-    # args = parser.parse_args()
-    # if args.coppeliasim_library == 'default':
-    #     defaultLibNameBase = 'coppeliaSim'
-    #     if args.true_headless:
-    #         defaultLibNameBase = 'coppeliaSimHeadless'
-    #     from pathlib import Path
-    #     # libPath = Path(__file__).absolute().parent
-    #     libPath = copp_lib_dir
-    #     import platform
-    #     plat = platform.system()
-    #     if plat == 'Windows':
-    #         libPath += defaultLibNameBase + '.dll'
-    #     elif plat == 'Linux':
-    #         libPath += 'lib' + defaultLibNameBase + '.so'
-    #     elif plat == 'Darwin':
-    #         libPath = libPath / '..' / 'MacOS' / 'lib' + defaultLibNameBase + '.dylib'
-    #     args.coppeliasim_library = str(libPath)
-    true_headless = False
-    defaultLibNameBase = 'coppeliaSim'
-    if true_headless:
-        defaultLibNameBase = 'coppeliaSimHeadless'
-    from pathlib import Path
-    libPath = Path(copp_lib_dir)
-
-    import platform
-    plat = platform.system()
-    if plat == 'Windows':
-        libPath /= defaultLibNameBase + '.dll'
-    elif plat == 'Linux':
-        libPath /= 'lib' + defaultLibNameBase + '.so'
-    elif plat == 'Darwin':
-        libPath = libPath / '..' / 'MacOS' / 'lib' + defaultLibNameBase + '.dylib'
-    coppeliasim_library = str(libPath)
-    
-    builtins.coppeliasim_library = coppeliasim_library
-    from coppeliasim.lib import *
-
-    headless = False
+def simulate(headless=False, true_headless=False):
+    from coppeliasim.lib import simRunGui
     sim_gui_all = 0x0ffff
     sim_gui_headless = 0x10000
     if headless or true_headless:
@@ -117,3 +70,29 @@ if __name__ == '__main__':
         t.start()
         simRunGui(options)
         t.join()
+
+def get_library(true_headless=False):
+    copp_lib_dir = "/home/nathan/Programs/CoppeliaSim/CoppeliaSim_Edu_V4_6_0_rev10_Ubuntu20_04/"
+    sys.path.append(copp_lib_dir)
+    defaultLibNameBase = 'coppeliaSim'
+    if true_headless:
+        defaultLibNameBase = 'coppeliaSimHeadless'
+    from pathlib import Path
+    libPath = Path(copp_lib_dir)
+
+    import platform
+    plat = platform.system()
+    if plat == 'Windows':
+        libPath /= defaultLibNameBase + '.dll'
+    elif plat == 'Linux':
+        libPath /= 'lib' + defaultLibNameBase + '.so'
+    elif plat == 'Darwin':
+        libPath = libPath / '..' / 'MacOS' / 'lib' + defaultLibNameBase + '.dylib'
+    coppeliasim_library = str(libPath)
+
+    import builtins
+    builtins.coppeliasim_library = coppeliasim_library
+
+if __name__ == '__main__':
+    get_library(true_headless=True)
+    simulate()
