600662a550ec218958fbc90237868b82a7ded888
Nathan <savardnm@gmail.com>
2024-05-15 11:32:42 +0200

    add find port file for stupid coppelias silly geese

---
+++ find_port.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3.8
+
+import socket
+from contextlib import closing
+from random import randint
+
+def find_free_port():
+    with closing(socket.socket(socket.AF_INET, socket.SOCK_STREAM)) as s:
+        s.bind(('', 0))
+        s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+        return s.getsockname()[1]
+
+
+print(find_free_port())
