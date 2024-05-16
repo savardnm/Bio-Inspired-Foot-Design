0dc55bd60849d323419a440657426ad68ee8f033
savardnm <savardnm@gmail.com>
2024-05-16 14:01:41 +0700

    results

--- src/claw_ga.py
+++ src/claw_ga.py
@@ -126,7 +126,7 @@ if __name__ == '__main__':
         population_range = (0x0,0xffffffff),
     )
 
-    # initial_population = population_from_csv("/home/nathan/Documents/GitHub/Bio-Inspired-Foot-Design/results/csv/ga_results_complex.csv")
+    initial_population = population_from_csv("/home/nathan/Documents/GitHub/Bio-Inspired-Foot-Design/results/csv/ga_results_complex05:15:2024:21:11:46.csv")
 
     objective_function = GripStrengthObjective(
         max_processes=12
