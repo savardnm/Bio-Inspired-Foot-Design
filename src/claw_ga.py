4501fe52dd1f37104e4baaa9749a3fa5a5107e0a
savardnm <savardnm@gmail.com>
2024-05-20 13:28:52 +0700

    100x100 results

--- src/claw_ga.py
+++ src/claw_ga.py
@@ -126,10 +126,10 @@ if __name__ == '__main__':
         population_range = (0x0,0xffffffff),
     )
 
-    initial_population = population_from_csv("/home/nathan/Documents/GitHub/Bio-Inspired-Foot-Design/results/csv/ga_results_complex05:15:2024:21:11:46.csv")
+    initial_population = population_from_csv("/home/nathan/Documents/GitHub/Bio-Inspired-Foot-Design/results/csv/ga_results_complex05:16:2024:13:59:13.csv")
 
     objective_function = GripStrengthObjective(
-        max_processes=12
+        max_processes=16
     )
 
     crossover_mechanism = SingleRandomCrossover(
